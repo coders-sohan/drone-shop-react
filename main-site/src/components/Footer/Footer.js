a2ea902d535ed28aa649c62945e77d47033a89b3
coders-sohan <coders.sohan.0216@gmail.com>
2022-04-27 20:26:34 +0600

    footer updated

--- main-site/src/components/Footer/Footer.js
+++ main-site/src/components/Footer/Footer.js
@@ -1,7 +1,22 @@
 import React from "react";
+import './Footer.css';
 
 const Footer = () => {
-	return <div className="py-20 bg-red-600">Footer</div>;
+  return (
+    <>
+      <div className="footer-sec flex justify-between items-center py-12">
+				<div className="container mx-auto px-3">
+					<div className="grid lg:grid-cols-6 md:grid-cols-2 grid-cols-1 m-auto gap-5">
+						<div className="p-2 bg-green-600"></div>
+						<div className="p-2 bg-green-300"></div>
+						<div className="p-2 bg-red-600"></div>
+						<div className="p-2 bg-red-300"></div>
+						<div className="p-2 md:col-span-2 col-span-1 bg-blue-600"></div>
+					</div>
+				</div>
+			</div>
+    </>
+  );
 };
 
 export default Footer;
