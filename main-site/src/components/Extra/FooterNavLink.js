ebbb4b1815088865c532f4bbfe0d12b47b2e60da
coders-sohan <coders.sohan.0216@gmail.com>
2022-04-27 23:01:45 +0600

    footer updated

---
+++ main-site/src/components/Extra/FooterNavLink.js
@@ -0,0 +1,96 @@
+export const quickLinks = [
+  {
+    path: "/",
+    name: "My Account",
+  },
+  {
+    path: "/about",
+    name: "About us",
+  },
+  {
+    path: "/shop",
+    name: "Shop",
+  },
+  {
+    path: "/blogs",
+    name: "Blogs",
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+  },
+];
+
+export const categories = [
+  {
+    path: "/",
+    name: "Shipping & taxes",
+  },
+  {
+    path: "/",
+    name: "Returns policy",
+  },
+  {
+    path: "/",
+    name: "Careens",
+  },
+  {
+    path: "/",
+    name: "Affiliates",
+  },
+  {
+    path: "/",
+    name: "Legal Notice",
+  },
+];
+
+export const informations = [
+  {
+    path: "/",
+    name: "New collection",
+  },
+  {
+    path: "/",
+    name: "Manufacturers",
+  },
+  {
+    path: "/",
+    name: "New products",
+  },
+  {
+    path: "https://template.hasthemes.com/droon/droon/index.html",
+    name: "Image credits",
+  },
+  {
+    path: "/",
+    name: "Delivery & returns",
+  },
+];
+
+export const socialLinks = [
+  {
+    path: "https://fb.com",
+    name: "Facebook",
+    iconClassName: "fa-brands fa-facebook-f",
+  },
+  {
+    path: "https://twitter.com",
+    name: "Twitter",
+    iconClassName: "fa-brands fa-twitter",
+  },
+  {
+    path: "https://instagram.com",
+    name: "Instagram",
+    iconClassName: "fa-brands fa-instagram",
+  },
+  {
+    path: "https://youtube.com",
+    name: "Youtube",
+    iconClassName: "fa-brands fa-youtube",
+  },
+  {
+    path: "https://pinterest.com",
+    name: "Pinterest",
+    iconClassName: "fa-brands fa-pinterest-p",
+  },
+];
--- main-site/src/components/Footer/Footer.js
+++ main-site/src/components/Footer/Footer.js
@@ -1,20 +1,139 @@
 import React from "react";
-import './Footer.css';
+import { Link } from "react-router-dom";
+import {
+  quickLinks,
+  categories,
+  informations,
+  socialLinks,
+} from "../Extra/FooterNavLink";
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <>
-      <div className="footer-sec flex justify-between items-center py-12">
-				<div className="container mx-auto px-3">
-					<div className="grid lg:grid-cols-6 md:grid-cols-2 grid-cols-1 m-auto gap-5">
-						<div className="p-2 bg-green-600"></div>
-						<div className="p-2 bg-green-300"></div>
-						<div className="p-2 bg-red-600"></div>
-						<div className="p-2 bg-red-300"></div>
-						<div className="p-2 md:col-span-2 col-span-1 bg-blue-600"></div>
-					</div>
-				</div>
-			</div>
+      <div className="footer-sec flex justify-between items-center pt-12 pb-10">
+        <div className="container mx-auto px-3">
+          <div className="grid lg:grid-cols-6 md:grid-cols-3 grid-cols-2 m-auto gap-3">
+            <div className="p-2">
+              <h4 className="mb-8 ml-4 text-black font-semibold text-xl">
+                Quick Links
+              </h4>
+              <ul className="footer-list">
+                {quickLinks.map((quickLink, index) => (
+                  <li key={quickLink.name + index}>
+                    <Link
+                      className="text-black hover:text-red-600 transition-all ease-linear"
+                      to={quickLink.path}
+                    >
+                      {quickLink.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="p-2">
+              <h4 className="mb-8 ml-4 text-black font-semibold text-xl">
+                Categories
+              </h4>
+              <ul className="footer-list">
+                {categories.map((categorie, index) => (
+                  <li key={categorie.name + index}>
+                    <Link
+                      className="text-black hover:text-red-600 transition-all ease-linear"
+                      to={categorie.path}
+                    >
+                      {categorie.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="p-2">
+              <h4 className="mb-8 ml-4 text-black font-semibold text-xl">
+                Information
+              </h4>
+              <ul className="footer-list">
+                {informations.map((information, index) => (
+                  <li key={information.name + index}>
+                    <Link
+                      className="text-black hover:text-red-600 transition-all ease-linear"
+                      to={information.path}
+                    >
+                      {information.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="p-2">
+              <h4 className="mb-8 ml-4 text-black font-semibold text-xl">
+                Follow Us On
+              </h4>
+              <ul className="social-list">
+                {socialLinks.map((socialLink, index) => (
+                  <li key={socialLink.name + index}>
+                    <Link
+                      className="text-black hover:text-red-600 transition-all ease-linear flex items-center"
+                      to={socialLink.path}
+                    >
+                      <span className="mr-5 mt-[1px]">
+                        <i className={socialLink.iconClassName}></i>
+                      </span>
+                      <span>{socialLink.name}</span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="p-2 md:col-span-2 col-span-2">
+              <h4 className="mb-8 text-black font-semibold text-xl">
+                About Info
+              </h4>
+              <p>Subscribe to our newsleter.</p>
+              <div className="flex py-3">
+                <input
+                  type="text"
+                  className="outline-none border border-black focus:border-red-600 p-3"
+                  placeholder="email@example.com"
+                />
+                <button className="rounded-none px-5 border-black bg-black hover:bg-red-600 hover:border-red-600 transition-all ease-linear">
+                  Subscribe
+                </button>
+              </div>
+              <p>* We'll never share your email address.</p>
+              <div className="pt-3">
+                <img
+                  className="w-72 h-auto"
+                  src="https://drone-shop-react.netlify.app/store.webp"
+                  alt="store"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-between items-center py-6 bg-gray-700">
+        <div className="container mx-auto px-3">
+          <div className="flex justify-between items-center md:flex-row flex-col">
+            <div className="text-white text-sm">
+              &copy; {Date().slice(11, 15)} Drone Shop. Made with ❤️ by{" "}
+              <a
+                href="https://sohan.pro"
+                className="text-yellow-400 hover:text-red-400 transition-all ease-linear"
+              >
+                Sohan
+              </a>
+            </div>
+            <div className="md:pt-0 pt-5">
+              <img
+                className="w-72 h-auto"
+                src="https://drone-shop-react.netlify.app/card.webp"
+                alt="cards"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
