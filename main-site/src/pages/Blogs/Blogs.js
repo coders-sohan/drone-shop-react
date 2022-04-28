0bc3195afabdfb4548dd3ccc9b57250c508a910c
coders-sohan <coders.sohan.0216@gmail.com>
2022-04-28 16:47:49 +0600

    blog page updated

--- main-site/src/pages/Blogs/Blogs.js
+++ main-site/src/pages/Blogs/Blogs.js
@@ -1,17 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 
 const Blogs = () => {
-	return (
-		<>
-			<Navbar />
-			<div className="h-96 py-20 text-5xl">
-				<h1>hello blogs</h1>
-			</div>
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <div className="flex justify-between items-center py-12">
+        <div className="container mx-auto px-3">
+          <div className="pt-16">
+            <h1 className="text-4xl">Blogs Will be added soon</h1>
+            <div className="bg-red-600 w-36 h-1 mt-10 mb-12"></div>
+            <div>
+              <Link
+                to="/home"
+                className="bg-red-600 text-white px-5 py-2 rounded-md text-xl"
+              >
+                Go to home
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Blogs;
